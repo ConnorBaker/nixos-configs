ca7e4bc85c9be5a6a8fc538580c3f5556e4265e7
Connor Baker <connor.baker@tweag.io>
2023-06-17 02:06:16 +0000

    move signing to nix to avoid 'you are not a trusted user' error

--- modules/common/nix-cantcacheme-upload.nix
+++ modules/common/nix-cantcacheme-upload.nix
@@ -3,6 +3,7 @@
   pkgs,
   ...
 }: {
+  nix.settings.secret-key-files = ["/run/keys/cantcacheme-signing-key"];
   queued-build-hook = {
     enable = true;
     postBuildScriptContent = ''
@@ -12,7 +13,6 @@
 
       echo "Uploading paths" $OUT_PATHS
       exec ${lib.getExe pkgs.nix} copy \
-        --extra-secret-key-files ''${CREDENTIALS_DIRECTORY}/cantcacheme-signing-key \
         --to "s3://cant-cache-me?endpoint=$CANTCACHEME_S3_ENDPOINT&compression=zstd&compression-level=19" \
         $OUT_PATHS
     '';
@@ -20,11 +20,8 @@
       AWS_ACCESS_KEY_ID = "/run/keys/cantcacheme-access-key";
       AWS_SECRET_ACCESS_KEY = "/run/keys/cantcacheme-secret-access-key";
       CANTCACHEME_S3_ENDPOINT = "/run/keys/cantcacheme-s3-endpoint";
-      cantcacheme-signing-key = "/run/keys/cantcacheme-signing-key";
     };
   };
-  systemd.services.async-nix-post-build-hook.serviceConfig = {
-    Environment = ["XDG_CACHE_HOME=/tmp/queued-build-hook"];
-  };
+  systemd.services.async-nix-post-build-hook.environment.XDG_CACHE_HOME = "/tmp/async-nix-post-build-hook";
 }
 
