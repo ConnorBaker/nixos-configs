3b6a9ffc916cb99de96a9c2ae6ddb9807af3de0f
Connor Baker <connor.baker@tweag.io>
2023-06-18 19:11:32 +0000

    use substituters by default

--- modules/cuda.nix
+++ modules/cuda.nix
@@ -1,8 +1,9 @@
 {
-  nix.settings = {
-    extra-substituters = [
-      "https://cuda-maintainers.cachix.org"
-    ];
+  nix.settings = let
+    substituters = ["https://cuda-maintainers.cachix.org"];
+  in {
+    extra-substituters = substituters;
+    extra-trusted-substituters = substituters;
     extra-trusted-public-keys = [
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
--- modules/nix/cantcacheme/pull.nix
+++ modules/nix/cantcacheme/pull.nix
@@ -1,8 +1,9 @@
 {
-  nix.settings = {
-    extra-substituters = [
-      "https://cantcache.me"
-    ];
+  nix.settings = let
+    substituters = ["https://cantcache.me"];
+  in {
+    extra-substituters = substituters;
+    extra-trusted-substituters = substituters;
     extra-trusted-public-keys = [
       "cantcache.me:Y+FHAKfx7S0pBkBMKpNMQtGKpILAfhmqUSnr5oNwNMs="
     ];
