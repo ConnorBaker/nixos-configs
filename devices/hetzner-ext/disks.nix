cc14c95c76b38e9b8821582969e125bbece5f860
Connor Baker <connor.baker@tweag.io>
2023-07-24 03:10:26 +0000

    try btrfs

--- devices/hetzner-ext/disks.nix
+++ devices/hetzner-ext/disks.nix
@@ -1,40 +1,3 @@
-# # Example to create a bios compatible gpt partition
-# {
-#   disko.devices.disk.vdb = {
-#     device = "/dev/disk/by-id/ata-ST16000NM003G-2KH113_ZL2AE5N5";
-#     type = "disk";
-#     content = {
-#       type = "gpt";
-#       partitions = {
-#         boot = {
-#           size = "2M";
-#           type = "EF02"; # for grub MBR
-#         };
-#         root = {
-#           size = "100%";
-#           content = {
-#             type = "btrfs";
-#             extraArgs = ["-f"]; # Override existing partition
-#             subvolumes = {
-#               # Subvolume name is different from mountpoint
-#               "/rootfs" = {
-#                 mountpoint = "/";
-#               };
-#               # Mountpoints inferred from subvolume name
-#               "/home" = {
-#                 mountOptions = ["compress=zstd"];
-#               };
-#               "/nix" = {
-#                 mountOptions = ["compress=zstd" "noatime"];
-#               };
-#               "/test" = {};
-#             };
-#           };
-#         };
-#       };
-#     };
-#   };
-# }
 {
   disko.devices.disk.main = {
     device = "/dev/disk/by-id/ata-ST16000NM003G-2KH113_ZL2AE5N5";
@@ -58,9 +21,21 @@
         root = {
           size = "100%";
           content = {
-            format = "ext4";
-            mountpoint = "/";
-            type = "filesystem";
+            type = "btrfs";
+            extraArgs = ["-f"];
+            subvolumes = {
+              # Subvolume name is different from mountpoint
+              "/rootfs" = {
+                mountpoint = "/";
+              };
+              # Mountpoints inferred from subvolume name
+              "/home" = {
+                mountOptions = ["compress=zstd"];
+              };
+              "/nix" = {
+                mountOptions = ["compress=zstd" "noatime"];
+              };
+            };
           };
         };
       };
