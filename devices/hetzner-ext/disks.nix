c16b39c36ced1189379ada315a308d542bf6f390
Connor Baker <connor.baker@tweag.io>
2023-07-25 04:49:22 +0000

    try zfs

--- devices/hetzner-ext/disks.nix
+++ devices/hetzner-ext/disks.nix
@@ -1,10 +1,17 @@
 {lib, ...}: let
-  # TODO(@connorbaker): Did using the full path name as the key cause failures?
-  # Error:
-  # mkfs.vfat: unable to open /dev/disk/by-partlabel/disk-_dev_disk_by-id_ata-ST16000NM003G-2KH113_ZL2AE5N5-ESP: No such file or directory
-  # Contents of /dev/disk/by-partlabel:
-  # disk-_dev_disk_by-id_ata-ST16000NM00
-  # Notice the truncated name.
+  # TODO(@connorbaker):
+  #
+  # We cannot use the full ID and default label naming scheme due to length limitations.
+  #
+  # For example, using the ID "ata-ST16000NM003G-2KH113_ZL2AE5N5" results in the following error:
+  #
+  # mkfs.vfat: unable to open /dev/disk/by-partlabel/disk-ata-ST16000NM003G-2KH113_ZL2AE5N5-ESP: No such file or directory
+  #
+  # Looking at the contents of /dev/disk/by-partlabel, we see the following:
+  #
+  # disk-ata-ST16000NM003G-2KH113_ZL2AE5
+  #
+  # Notice the truncated name! This is why we must specify the label ourselves.
   diskIds = [
     "ata-ST16000NM003G-2KH113_ZL2AE5N5"
     "ata-ST16000NM003G-2KH113_ZL2BTF3N"
@@ -19,10 +26,12 @@
       type = "gpt";
       partitions = {
         boot = {
+          label = "${diskId}-boot";
           size = "1M";
           type = "EF02"; # for grub MBR
         };
         ESP = {
+          label = "${diskId}-ESP";
           size = "512M";
           type = "EF00";
           content = {
@@ -41,6 +50,7 @@
       type = "gpt";
       partitions = {
         zfs = {
+          label = "${diskId}-zfs";
           size = "100%";
           content = {
             type = "zfs";
