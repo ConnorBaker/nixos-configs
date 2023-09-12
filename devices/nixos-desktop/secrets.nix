fae4dafdf8c45874c8a904917610fc9f34b8aa3f
Connor Baker <connor.baker@tweag.io>
2023-09-12 22:37:24 +0000

    secrets: wip

--- devices/nixos-desktop/secrets.nix
+++ devices/nixos-desktop/secrets.nix
@@ -2,9 +2,6 @@
   sops = {
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     defaultSopsFile = ./secrets.yaml;
-    secrets = {
-      "ssh/ssh_host_rsa_key".path = "/etc/ssh/ssh_host_rsa_key";
-      "tailscale/tskey_reusable".path = "/etc/tailscale/tskey-reusable";
-    };
+    secrets."tailscale/tskey_reusable".path = "/etc/tailscale/tskey-reusable";
   };
 }
