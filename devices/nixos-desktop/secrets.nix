8bac99a3efbbaef385cf6482d1a61a04a8b136a4
Connor Baker <connor.baker@tweag.io>
2023-09-13 01:41:20 +0000

    secrets: wip

--- devices/nixos-desktop/secrets.nix
+++ devices/nixos-desktop/secrets.nix
@@ -1,5 +1,6 @@
 {
   sops = {
+    # TODO: Switch to /persist/etc when moving to impermanence.
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     defaultSopsFile = ./secrets.yaml;
     gnupg.sshKeyPaths = [];
