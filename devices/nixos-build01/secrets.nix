147dddc4f6da078d2070e2a71b3cc3873f132447
Connor Baker <connor.baker@tweag.io>
2023-09-13 03:00:29 +0000

    secrets: wip

--- devices/nixos-build01/secrets.nix
+++ devices/nixos-build01/secrets.nix
@@ -1,6 +1,6 @@
 {
   sops = {
-    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    age.sshKeyPaths = ["/persist/etc/ssh/ssh_host_ed25519_key"];
     defaultSopsFile = ./secrets.yaml;
     gnupg.sshKeyPaths = [];
     secrets = {
