5fc7439f278cac2abab6859033a0ea7e7df74e3a
Connor Baker <connor.baker@tweag.io>
2023-12-08 23:32:31 +0000

    add HCI session keys so device are recognized on rejoin

--- devices/nixos-ext/secrets.nix
+++ devices/nixos-ext/secrets.nix
@@ -7,20 +7,26 @@
       let
         # Named after their paths in secrets.yaml.
         tsKey = "tailscale/tskey-reusable";
-        hciKey = "hercules-ci-agent/secrets/cluster-join-token.key";
         hciBinaryCaches = "hercules-ci-agent/secrets/binary-caches.json";
+        hciClusterKey = "hercules-ci-agent/secrets/cluster-join-token.key";
+        hciSessionKey = "hercules-ci-agent/secretState/session.key";
       in
       {
         ${tsKey}.path = "/etc/${tsKey}";
-        ${hciKey} = {
-          owner = "hercules-ci-agent";
-          mode = "0440";
-          path = "/var/lib/${hciKey}";
-        };
         ${hciBinaryCaches} = {
           owner = "hercules-ci-agent";
           mode = "0440";
           path = "/var/lib/${hciBinaryCaches}";
+        };
+        ${hciClusterKey} = {
+          owner = "hercules-ci-agent";
+          mode = "0440";
+          path = "/var/lib/${hciClusterKey}";
+        };
+        ${hciSessionKey} = {
+          owner = "hercules-ci-agent";
+          mode = "0440";
+          path = "/var/lib/${hciSessionKey}";
         };
       };
   };
