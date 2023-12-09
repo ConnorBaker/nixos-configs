628cc2b934451dba24387bfb2ba1a7af77ba5d63
Connor Baker <connor.baker@tweag.io>
2023-12-09 00:19:25 +0000

    finish removed aarch64-linux qemu

--- devices/nixos-build01/hardware.nix
+++ devices/nixos-build01/hardware.nix
@@ -3,7 +3,6 @@
   imports = ["${modulesPath}/installer/scan/not-detected.nix"];
 
   boot = {
-    binfmt.emulatedSystems = ["aarch64-linux"];
     initrd.availableKernelModules = [
       "ahci"
       "nvme"
