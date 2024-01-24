67ce289331785c9fc320415528905e7d46112c0a
Connor Baker <connor.baker@tweag.io>
2024-01-24 20:45:03 +0000

    add ca-derivations cache

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -91,9 +91,16 @@ in
         "parse-toml-timestamps"
         "verified-fetches"
       ];
-      extra-substituters = ["https://cuda-maintainers.cachix.org"];
-      extra-trusted-substituters = ["https://cuda-maintainers.cachix.org"];
+      extra-substituters = [
+        "https://cache.ngi0.nixos.org"
+        "https://cuda-maintainers.cachix.org"
+      ];
+      extra-trusted-substituters = [
+        "https://cache.ngi0.nixos.org"
+        "https://cuda-maintainers.cachix.org"
+      ];
       extra-trusted-public-keys = [
+        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
       fsync-metadata = false;
