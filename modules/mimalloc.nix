86f420527b561d0296b4d50a8477d9247fa70a63
Connor Baker <connor.baker@tweag.io>
2024-03-01 06:36:42 +0000

    tune for ZRAM usage

--- modules/mimalloc.nix
+++ modules/mimalloc.nix
@@ -4,6 +4,7 @@
 
   nixpkgs.overlays = lib.lists.optionals config.networking.dhcpcd.enable [
     # Must be disabled to use mimalloc
+    # https://github.com/NixOS/nixpkgs/blob/244ee5631a7a39b0c6bd989cdf9a1326cd3c5819/nixos/modules/services/networking/dhcpcd.nix#L212-L224
     (_: prev: { dhcpcd = prev.dhcpcd.override { enablePrivSep = false; }; })
   ];
 }
--- modules/zram.nix
+++ modules/zram.nix
@@ -1,7 +1,23 @@
 {
+  # Tune the configuration to take advantage of the ZRAM swap device.
+  boot.kernel.sysctl = {
+    # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+    # https://github.com/pop-os/default-settings/blob/master_noble/etc/sysctl.d/10-pop-default-settings.conf
+    "vm.swappiness" = 180; # Strong preference for ZRAM
+    "vm.max_map_count" = 2147483642;
+    # https://github.com/redhat-performance/tuned/blob/master/profiles/throughput-performance/tuned.conf
+    "vm.dirty_ratio" = 40;
+    "vm.dirty_background_ratio" = 10;
+  };
+
   zramSwap = {
     algorithm = "zstd";
     enable = true;
     memoryPercent = 200;
+    # TODO: Consider a writeback device?
+    # https://wiki.archlinux.org/title/Zram#Enabling_a_backing_device_for_a_zram_block
   };
 }
