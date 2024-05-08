f8f21ed0856e51e8e2bdb759976c856a0c2e9d6c
Connor Baker <connorbaker01@gmail.com>
2024-05-08 13:01:00 -0400

    deprecations

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,16 +1,13 @@
 { pkgs, ... }:
 {
+  hardware.nvidia-container-toolkit.enable = true;
   virtualisation = {
-    containers = {
-      enable = true;
-      cdi.dynamic.nvidia.enable = true;
-    };
+    containers.enable = true;
     docker = {
       enable = true;
       # CDI is feature-gated and only available from Docker 25 and onwards
       package = pkgs.docker_25;
       daemon.settings.features.cdi = true;
-      storageDriver = "zfs";
     };
     oci-containers.backend = "docker";
   };
