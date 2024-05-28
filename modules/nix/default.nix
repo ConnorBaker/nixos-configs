8d3957770539a34240e979bf0df4b1eacd6e6792
Connor Baker <connor.baker@tweag.io>
2024-05-28 18:47:22 +0000

    nix: binary cache failures should not be fatal

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -105,6 +105,7 @@ in
         "cuda:NtbpAU7XGYlttrhCduqvpYKottCPdWVITWT+3nFVTBY="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
+      fallback = true;
       fsync-metadata = false;
       http-connections = 256;
       max-substitution-jobs = 128;
