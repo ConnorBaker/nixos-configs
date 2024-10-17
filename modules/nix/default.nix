e8299e2ac0e0befeecffd32d6bb1acc44f9acbee
Connor Baker <ConnorBaker01@gmail.com>
2024-10-17 19:54:08 +0000

    nix: change default settings

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -91,6 +91,10 @@ in
       auto-optimise-store = false; # We wipe them frequently enough we don't need the performance hit.
       builders-use-substitutes = true;
       connect-timeout = 5; # Don't wait forever for a remote builder to respond.
+      # Since these machines are builders for CUDA packages, makes sense to allow a larger buffer for curl because we
+      # have lots of memory and will be downloading large tarballs.
+      # NOTE: https://github.com/NixOS/nix/pull/11171
+      download-buffer-size = "256M";
       experimental-features = [
         "auto-allocate-uids"
         "cgroups"
@@ -112,7 +116,10 @@ in
       fallback = true;
       fsync-metadata = false;
       http-connections = 256;
-      max-substitution-jobs = 128;
+      log-lines = 100;
+      max-substitution-jobs = 64;
+      # See: https://github.com/NixOS/nix/blob/1cd48008f0e31b0d48ad745b69256d881201e5ee/src/libstore/local-store.cc#L1172
+      nar-buffer-size = "1G";
       require-drop-supplementary-groups = true;
       system-features =
         hostNameToConfig.${config.networking.hostName}.supportedFeatures or baselineSupportedFeatures;
